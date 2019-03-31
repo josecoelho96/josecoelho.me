d944d4849be28a68614450e2a069483ba1062a2a
José Coelho <josecoelho96@gmail.com>
2019-03-31 17:09:14 +0100

    Add navbar

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <v-app>
+    <Navbar />
     <v-content>
       <router-view></router-view>
     </v-content>
@@ -7,9 +8,13 @@
 </template>
 
 <script>
+import Navbar from "@/components/Navbar.vue";
+
 export default {
   name: "App",
-  components: {},
+  components: {
+    Navbar
+  },
   data() {
     return {
       //
---
+++ src/components/Navbar.vue
@@ -0,0 +1,94 @@
+<template>
+  <nav>
+    <v-toolbar app dark color="primary">
+      <v-toolbar-side-icon @click="isDrawOpen = !isDrawOpen" />
+      <router-link :to="{ name: 'home' }" class="white--text">
+        <v-toolbar-title class="headline text-uppercase">
+          <span class="font-weight-light">{{ name.firstName }}</span>
+          <span class="font-weight-bold">{{ name.lastName }}</span>
+        </v-toolbar-title>
+      </router-link>
+      <v-spacer />
+      <v-toolbar-items class="hidden-sm-and-down">
+        <v-btn
+          v-for="option in menuOptions"
+          :key="option.option"
+          flat
+          :to="{ name: option.routeName }"
+          exact
+        >
+          {{ option.text }}
+        </v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
+    <v-navigation-drawer v-model="isDrawOpen" app temporary fixed>
+      <v-list class="pa-1">
+        <v-list-tile :to="{ name: 'home' }">
+          <v-list-tile-content>
+            <v-list-tile-title class="text-uppercase secondary--text">
+              {{ name.firstName }} {{ name.lastName }}</v-list-tile-title
+            >
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+
+      <v-list class="pt-0" dense>
+        <v-divider />
+        <v-list-tile
+          v-for="option in menuOptions"
+          :key="option.option"
+          :to="{ name: option.routeName }"
+          exact
+        >
+          <v-list-tile-action class="secondary--text">
+            <v-icon>{{ option.icon }}</v-icon>
+          </v-list-tile-action>
+
+          <v-list-tile-content class="secondary--text">
+            <v-list-tile-title>{{ option.text }}</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+  data: function() {
+    return {
+      menuOptions: [
+        {
+          option: "home",
+          text: "Home",
+          routeName: "home",
+          icon: "home"
+        },
+        {
+          option: "about",
+          text: "About",
+          routeName: "about",
+          icon: "person"
+        },
+        {
+          option: "contact",
+          text: "Contact",
+          routeName: "contact",
+          icon: "email"
+        }
+      ],
+      isDrawOpen: false,
+      firstName: "José",
+      name: { firstName: "José", lastName: "Coelho" }
+    };
+  }
+};
+</script>
+
+<style>
+a {
+  color: inherit;
+  text-decoration: inherit;
+}
+</style>
