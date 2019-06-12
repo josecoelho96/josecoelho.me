2c9f9260d978e17b9a96c353d3e26503a107a0cf
José Coelho <josecoelho96@gmail.com>
2019-06-12 21:48:15 +0100

    update contact view

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,3 +1,34 @@
 <template>
-  <v-content>Hello [Contact.vue]!</v-content>
+  <v-content>
+    <v-container id="top" text-xs-center>
+      <h1 class="display-1 mb-5">Contact me...</h1>
+      <v-layout row wrap>
+        <v-flex xs12 sm6 text-xs-center text-sm-right>
+          <v-avatar size="140">
+            <v-img
+              class="elevation-6 mr-3"
+              src="/img/photo.jpg"
+              aspect-ratio="1"
+            ></v-img>
+          </v-avatar>
+        </v-flex>
+        <v-flex xs12 sm6 text-xs-center text-sm-left>
+          <div>
+            <div class="mt-2">
+              <v-icon class="mr-2">person</v-icon>José Coelho
+            </div>
+            <div class="mt-2">
+              <v-icon class="mr-2">cake</v-icon>April 3rd, 1996
+            </div>
+            <div class="mt-2">
+              <v-icon class="mr-2">email</v-icon>me@josecoelho.me
+            </div>
+            <div class="mt-2">
+              <v-icon class="mr-2">location_on</v-icon>Lisbon, Portugal
+            </div>
+          </div>
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </v-content>
 </template>
