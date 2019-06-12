fb49dc398178abaa834f8d94d806f399415ca054
José Coelho <josecoelho96@gmail.com>
2019-06-12 22:24:39 +0100

    add basic home view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,3 +1,20 @@
 <template>
-  <v-content>Hello [Home.vue]!</v-content>
+  <v-content>
+    <v-container id="top" text-xs-center>
+      <h1 class="display-1">Hello! Welcome!</h1>
+      <p>Welcome to my personal webpage</p>
+      <div>
+        <v-btn round :to="{ name: 'about' }" color="primary" dark>Read more about me</v-btn>
+      </div>
+      <div>
+        <v-btn round :to="{ name: 'contact' }" color="primary" dark>My contacts</v-btn>
+      </div>
+    </v-container>
+  </v-content>
 </template>
+
+<script>
+export default {
+  name: "Home",
+};
+</script>
